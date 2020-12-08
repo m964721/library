22063fa695d13f7180ddb73573aa452e207f521d
hanpengfei <>
2020-12-08 10:38:41 +0800

    删除多余文件

--- app/src/main/java/com/demo/application/mvp/base/BasePresenter.java
+++
@@ -1,9 +0,0 @@
-package com.demo.application.mvp.base;
-
-import java.lang.ref.WeakReference;
-
-//P中包含v和m，通过P实现数据交换
-public class BasePresenter {
-    protected BaseModel baseModel ;
-    protected WeakReference<BaseModeToView> weakReference ;//弱引用
-}
